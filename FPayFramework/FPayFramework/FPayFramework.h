b4bc2dc42a31886d992220556e04a380c3e6229e
TungHD <tunghd.uet31@gmail.com>
2021-12-28 16:09:45 +0700

    Initialize FPay Framework

---
+++ FPayFramework/FPayFramework/FPayFramework.h
@@ -0,0 +1,18 @@
+//
+//  FPayFramework.h
+//  FPayFramework
+//
+//  Created by Hoàng Tùng on 28/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FPayFramework.
+FOUNDATION_EXPORT double FPayFrameworkVersionNumber;
+
+//! Project version string for FPayFramework.
+FOUNDATION_EXPORT const unsigned char FPayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPayFramework/PublicHeader.h>
+
+
---
+++ FPayFramework/FPayFramework/Services.swift
@@ -0,0 +1,14 @@
+//
+//  Services.swift
+//  FPayFramework
+//
+//  Created by Hoàng Tùng on 28/12/2021.
+//
+
+import Foundation
+public class Services {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Hello from FPay Framework"
+    }
+}
